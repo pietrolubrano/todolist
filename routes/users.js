cc0306a728d0a6c4956ea5bdc5a25a4f4a862a3f
Pietro <pietrolubrano86@gmail.com>
2022-02-22 23:14:43 +0100

    salted password with bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
-
+const bcrypt = require('bcrypt')
 const User = require('../models/User');
 
 router.post('/',
@@ -9,29 +9,33 @@ router.post('/',
     body('email').isEmail().withMessage('Inserisci una mail valida'),
     body('password').isLength({ min: 5 }).withMessage('Inserisci una password valida'),
 
-    (req, res) => {
+    async (req, res) => {
+        const { name, email, password } = req.body
+
+        const checkEmail = await User.findOne({ email })
+
+        if(checkEmail){
+            const error = {
+                value: email,
+                msg: "Questa mail è gia registrata",
+                param: "email",
+                location: "body"
+            }
+            return res.status(400).json({ errors: [error] });
+        }
+
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-        const { name, email, password } = req.body
+        
 
-        User.create({ name, email, password })
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        User.create({ name, email, password: hashedPassword })
             .then(user => res.json(user))
-            .catch(err => {
-                if(err.keyValue['email']){
-                    const error = {
-                        value: email,
-                        msg: "Questa mail è gia registrata",
-                        param: "email",
-                        location: "body"
-                    }
-                    
-                    console.log(err.keyValue)
-                    return res.status(400).json({ errors: [error] });
-                }
-                return res.status(400).json(err);
-            })
+            .catch(err => res.status(400).json(err))
     }
 )
 
