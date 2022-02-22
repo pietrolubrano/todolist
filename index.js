e9665d105b57a7d5ad788759d10dd222b276206e
Pietro <pietrolubrano86@gmail.com>
2022-02-22 17:54:49 +0100

    added dotenv package and connection variables

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
 
 const app = express()
 
+mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING)
+    .then( res => console.log('Connected to db'))
+    .catch(err => console.log(err))
+
+const listener = app.listen(
+    process.env.APP_LISTENING_PORT || 4000,
+    () => console.log(`Listening on port: ${listener.address().port}`)
+)
+
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/users', require('./routes/auth'))
-app.listen(4000)
 
 app.get('/', (req, res) => {
     console.log('request')
