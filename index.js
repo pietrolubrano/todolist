d4b33f7c6ef64f812a07d58f4d96ad805484eca5
Pietro <pietrolubrano86@gmail.com>
2022-02-22 20:27:15 +0100

    added middleware to accept body in request

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ const app = express()
 
 mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING)
     .then( res => console.log('Connected to db'))
-    .catch(err => console.log(err))
+    .catch( err => console.log(err) )
 
 const listener = app.listen(
     process.env.APP_LISTENING_PORT || 4000,
     () => console.log(`Listening on port: ${listener.address().port}`)
 )
+
+// Init Middleware -> per accettare i dati nel body della request
+app.use(express.json({ extended: false }))
 
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/users', require('./routes/auth'))
