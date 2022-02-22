fcca27c38a566c72d7cf2afcefa1add44346da1c
Pietro <pietrolubrano86@gmail.com>
2022-02-22 20:26:47 +0100

    added user model

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const user = require('../models/User');
+
 router.post('/', (req, res) => {
     res.send('register a user')
 })
